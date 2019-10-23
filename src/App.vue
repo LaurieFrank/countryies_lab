f0f0c923bfb9603d8bfaa66a0a4f15e6904b5f3a
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-23 15:25:23 +0100

    mvp completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template lang="html">
   <div class="">
+    <h1>Countries</h1>
     <countries-list :countries="countries"></countries-list>
+    <country-detail :country="selectedCountry"></country-detail>
   </div>
 </template>
 
 <script>
-importing CountriesList from './components/CountriesList.vue';
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
 import {eventBus} from './main.js'
 
 export default {
@@ -22,10 +25,11 @@ mounted(){
   });
   fetch('https://restcountries.eu/rest/v2/all')
   .then(res => res.json())
-  .then(countries => this.countries = contries)
+  .then(data => this.countries = data)
   },
   components: {
-    "countries-list": CountriesList
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div v-if='country'>
+    <h3>{{ country.name }}</h3>
+    <p>population: {{ country.population }}</p>
+    <p>capital: {{ country.capital }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "country-detail",
+  props: ["country"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
